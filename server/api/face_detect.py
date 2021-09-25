d21ea2f68d35c592a46af218bb5d0fcdb77a8dbf
stepan14041999 <stepan.14041999@gmail.com>
2021-09-25 17:11:38 +0400

    add infos

--- server/api/face_detect.py
+++ server/api/face_detect.py
@@ -1,18 +1,37 @@
-import os
+import time
 from datetime import datetime
-
-from flask import make_response
-from werkzeug.datastructures import FileStorage
+import random
 
 from facenet_pytorch import MTCNN
 import torch
-import numpy as np
-import mmcv, cv2
 from PIL import Image
+import base64
 
 from api import base
+from flask import jsonify
 
 from nn.MTCNN import detect_faces_on_frame
+
+
+def str_time_prop(start, end, time_format, prop):
+    """Get a time at a proportion of a range of two formatted times.
+
+    start and end should be strings specifying times formatted in the
+    given format (strftime-style), giving an interval [start, end].
+    prop specifies how a proportion of the interval to be taken after
+    start.  The returned time will be in the specified format.
+    """
+
+    stime = time.mktime(time.strptime(start, time_format))
+    etime = time.mktime(time.strptime(end, time_format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(time_format, time.localtime(ptime))
+
+
+def random_date(start, end, prop):
+    return str_time_prop(start, end, '%m/%d/%Y %I:%M %p', prop)
 
 
 class FaceDetection(base.ApiResource):
@@ -25,14 +44,16 @@ class FaceDetection(base.ApiResource):
 
         with Image.open("screen.jpg") as frame:
             coords = detect_faces_on_frame(frame, mtcnn)
-        
-        l_coords = list()
-        for coord in coords:
-            l_coords.append(coord.tolist())
 
-        dt = str(datetime(year=2021, month=9, day=11, hour=14))
+        with open("screen.jpg", "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
 
-        return {'coords': l_coords, 'datetime': dt}
+        clients = dict()
+
+        for idx, coord in enumerate(coords):
+            clients[idx] = {'coords': coord.tolist(), 'datetime': random_date("1/1/2018 1:30 PM", "1/1/2022 4:50 AM", random.random()), 'rating': random.uniform(1, 5)}
+
+        return {'clients': clients, 'source_image_base_64': encoded_string.decode('utf-8')}
 
     def post(self):
         return self._dataset()
