5d1606e12210bd665c935cec27081d4929780e55
Artyom Davidovich <dsda_90@mail.ru>
2021-09-25 10:48:15 +0400

    Base.

---
+++ client/src/components/Card.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <div class="container-avatar">
+      <img src="../assets/rabbit_1.png" class="rabbit" />
+
+      <!--      <img src="../assets/face.png" class="face" />-->
+      <img :src="source" class="face" />
+    </div>
+    <div class="date">Дата</div>
+    <div class="stars">
+      <star />
+      <star />
+      <star />
+      <star />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { Box } from "@/types";
+import Star from "@/components/Star.vue";
+
+export default defineComponent({
+  name: "Card",
+  components: { Star },
+  props: {
+    source: {
+      type: String,
+      required: true,
+    },
+    box: {
+      type: Object as PropType<Box>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 450px;
+  height: auto;
+  box-sizing: border-box;
+  color: white;
+  user-select: none;
+}
+
+.container-avatar {
+  width: 450px;
+  height: 450px;
+  border-radius: 50%;
+  box-sizing: border-box;
+  position: relative;
+  background: transparent; //#f5f5f5;
+}
+
+.rabbit {
+  position: absolute;
+  top: 0;
+  left: calc(50% - 130px);
+  height: 100%;
+  width: auto;
+  z-index: 100;
+}
+
+.face {
+  position: absolute;
+  top: 210px;
+  left: 180px;
+  width: 80px;
+  height: 80px;
+  z-index: 1;
+}
+</style>
---
+++ client/src/views/Schema.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="schema-wrapper">
+      <img src="../assets/schema_1.png" alt="Schema" class="schema" />
+      <img src="../assets/schema_2.png" alt="Schema" class="schema" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Schema",
+});
+</script>
+
+<style lang="scss">
+.container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.schema-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.schema {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  user-select: none;
+}
+</style>
